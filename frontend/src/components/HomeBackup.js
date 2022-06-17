1a322f5ce805d40267656e19fdc6295700062017
py <pyd3.14@gmail.com>
2022-06-17 22:02:39 +0200

    move components to folders

--- frontend/src/components/HomeBackup.js
+++ frontend/src/components/HomeBackup.js
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react'
 
 import Users from './Users';
-import Posts from './Posts';
+import Posts from './Posts/PostsPosts';
 import Entity from './Entity';
 
 import Entities from './Entities';
 
 const Home = () => {
-
+    const handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    const { activeItem } = useState();
     return (
-        <div>
-            <nav className="navbar">
+        <>
+            <Menu>
                 <Link
-                    className={"navbar-brand"} to={"/"}> Symfony React Project </Link>
+                    className={"nav-link"} to={"/posts"}>
+                    <Menu.Item
+                        name='posts'
+                        active={activeItem === 'posts'}
+                        onClick={handleItemClick}
+                    >
+                    </Menu.Item>
+                </Link>
+
                 <Link
-                    className={"nav-link"} to={"/posts"}> Posts </Link>
+                    className={"nav-link"} to={"/users"}>
+                    <Menu.Item
+                        name='users'
+                        active={activeItem === 'users'}
+                        onClick={handleItemClick}
+                    >
+                    </Menu.Item>
+                </Link>
+
                 <Link
-                    className={"nav-link"} to={"/users"}> Users </Link>
+                    className={"nav-link"} to={"/entity"}>
+                    <Menu.Item
+                        name='entity-1'
+                        active={activeItem === 'entity-1'}
+                        onClick={handleItemClick}
+                    >
+                    </Menu.Item>
+                </Link>
+
                 <Link
-                    className={"nav-link"} to={"/entity"}> Entity 1 </Link>
-                <Link
-                    className={"nav-link"} to={"/entities"}> Entities </Link>
-            </nav>
+                    className={"nav-link"} to={"/entities"}>
+                    <Menu.Item
+                        name='entities'
+                        active={activeItem === 'entities'}
+                        onClick={handleItemClick}
+                    >
+                    </Menu.Item>
+                </Link>
+            </Menu>
 
             <Routes>
                 <Route path="/users" element={<Users />} />
@@ -30,7 +61,7 @@ const Home = () => {
                 <Route path="/entity" element={<Entity />} />
                 <Route path="/entities" element={<Entities />} />
             </Routes>
-        </div>
+        </>
     )
 }
 
