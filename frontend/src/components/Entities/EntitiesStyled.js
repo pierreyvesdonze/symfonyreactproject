92c41d7e1d3e100ed89032e9e8283116748da085
py <pyd3.14@gmail.com>
2022-06-17 22:38:29 +0200

    Use styled-components on entities

---
+++ frontend/src/components/Entities/EntitiesStyled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const EntitiesStyled = styled.div`
+  max-width: 700px;
+  min-width: 400px;
+  width: 60%;
+  margin: auto;
+  background-color: #c4cecc;
+  height: 100vh;
+  box-shadow: 0 0 3em rgba(0, 0, 0, .4);
+  display: flex;
+  flex-direction: column;
+`;
+
+export default EntitiesStyled;
